020e20a691a25addec8d2674fad63f6d501b6a0e
Jacob Stoebel <stoebelj@berea.edu>
2016-11-17 20:59:50 -0500

    start on tests

--- test/models/user_model_test.js
+++ test/models/user_model_test.js
@@ -3,16 +3,16 @@ var should = chai.should();
 var models = require('require.all')('../../models')
 
 describe('User Model', function() {
-  it('should create a new user', function(done) {
-    var user = new models.User({
-      email: 'test@gmail.com',
-      password: 'password'
-    });
-    user.save(function(err) {
-      if (err) return done(err);
-      done();
-    });
-  });
+  // it('should create a new user', function(done) {
+  //   var user = new models.User({
+  //     email: 'test@gmail.com',
+  //     password: 'password'
+  //   });
+  //   user.save(function(err) {
+  //     if (err) return done(err);
+  //     done();
+  //   });
+  // });
 
   it('should not create a user with the unique email', function(done) {
     var user = new models.User({
@@ -25,18 +25,18 @@ describe('User Model', function() {
     });
   });
 
-  it('should find user by email', function(done) {
-    models.User.findOne({ email: 'test@gmail.com' }, function(err, user) {
-      if (err) return done(err);
-      user.email.should.equal('test@gmail.com');
-      done();
-    });
-  });
-
-  it('should delete a user', function(done) {
-    models.User.remove({ email: 'test@gmail.com' }, function(err) {
-      if (err) return done(err);
-      done();
-    });
-  });
+  // it('should find user by email', function(done) {
+  //   models.User.findOne({ email: 'test@gmail.com' }, function(err, user) {
+  //     if (err) return done(err);
+  //     user.email.should.equal('test@gmail.com');
+  //     done();
+  //   });
+  // });
+  //
+  // it('should delete a user', function(done) {
+  //   models.User.remove({ email: 'test@gmail.com' }, function(err) {
+  //     if (err) return done(err);
+  //     done();
+  //   });
+  // });
 });
