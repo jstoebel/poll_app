28c1a45d227bffa416a0f776cb275620644d106f
Jacob Stoebel <stoebelj@berea.edu>
2016-11-29 21:00:23 -0500

    more webpack setup

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ var upload = multer({ dest: path.join(__dirname, 'uploads') });
 var httpProxy = require('http-proxy');
 var proxy = httpProxy.createProxyServer();
 var config = require('./config/config')
+var httpProxy = require('http-proxy');
+
 
 
 /**
@@ -114,6 +116,35 @@ app.use('/', require('./config/routes'))
  * Error Handler.
  */
 app.use(errorHandler());
+
+
+// We only want to run the workflow when not in production
+var isProduction = process.env.NODE_ENV === 'production';
+var proxy = httpProxy.createProxyServer();
+if (!isProduction) {
+
+  // We require the bundler inside the if block because
+  // it is only needed in a development environment. Later
+  // you will see why this is a good idea
+  var bundle = require('./bundle.js');
+  bundle();
+
+  // Any requests to localhost:3000/build is proxied
+  // to webpack-dev-server
+  app.all('/build/*', function (req, res) {
+    proxy.web(req, res, {
+        target: 'http://localhost:8080'
+    });
+  });
+
+}
+
+// It is important to catch any errors from the proxy or the
+// server will crash. An example of this is connecting to the
+// server when webpack is bundling
+proxy.on('error', function(e) {
+  console.log('Could not connect to proxy, please try again...');
+});
 
 /**
  * Start Express server.
--- public/js/client.js
+++ public/js/client.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route, browserHistory } from 'react-router'
 import App from './components/App'
 import About from './components/About'
 import Repos from './components/Repos'
 
 render((
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={App}/>
     <Route path="/repos" component={Repos}/>
     <Route path="/about" component={About}/>
